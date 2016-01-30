e78a88734d9d27552d9602d8a03a5dcc0b210048
Cesar Eduardo Barros <cesarb@cesarb.eti.br>
2016-01-30 19:23:41 -0200

    Make the optimizer elide unnecessary bounds checks
    Surprisingly, this also made the optimizer autovectorize the code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
 // This function is non-inline to prevent the optimizer from looking inside it.
 #[inline(never)]
 fn constant_time_ne(a: &[u8], b: &[u8]) -> u8 {
-	// Compares the sizes here so the optimizer knows a.len() == b.len()
-	if a.len() != b.len() { return !0; }
+    assert!(a.len() == b.len());
+
+    // These useless slices make the optimizer elide the bounds checks.
+    // See the comment in clone_from_slice() added on Rust commit 6a7bc47.
+    let len = a.len();
+    let a = &a[..len];
+    let b = &b[..len];
 
 	let mut tmp = 0;
-	for i in 0..a.len() {
+	for i in 0..len {
 		tmp |= a[i] ^ b[i];
-		// Ideally, should use an inline asm here:
-		// asm!("" : "=r" (tmp) : "0" (tmp));
-		// But asm! is not stable yet.
-		// With the inline asm, this function could be inlined
-		// since the asm would prevent the optimizer from doing
-		// an early return.
 	}
 	tmp	// The compare with 0 must happen outside this function.
 }
@@ -34,5 +33,5 @@ fn constant_time_ne(a: &[u8], b: &[u8]) -> u8 {
 /// ```
 #[inline]
 pub fn constant_time_eq(a: &[u8], b: &[u8]) -> bool {
-	constant_time_ne(a, b) == 0
+	a.len() == b.len() && constant_time_ne(a, b) == 0
 }
