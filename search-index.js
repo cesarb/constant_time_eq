a0ab3d10e0a7b44fa9ffb4cfb9e1847bed16db19
Cesar Eduardo Barros <cesarb@cesarb.eti.br>
2016-01-30 19:54:41 -0200

    Initial commit

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['constant_time_eq'] = {"items":[[5,"constant_time_eq","constant_time_eq","Compares two equal-sized byte strings in constant time.",null,null]],"paths":[]};
+initSearch(searchIndex);
