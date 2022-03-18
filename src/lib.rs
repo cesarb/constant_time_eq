b749d4349186d87d5afaa519e88819119a24c83c
Cesar Eduardo Barros <cesarb@cesarb.eti.br>
2022-03-18 19:53:34 -0300

    Use inline asm when available to hide from optimizer

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,46 @@
 #![no_std]
 
-// This function is non-inline to prevent the optimizer from looking inside it.
-#[inline(never)]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[inline]
+fn optimizer_hide(mut value: u8) -> u8 {
+    // SAFETY: the input value is passed unchanged to the output, the inline assembly does nothing.
+    unsafe {
+        core::arch::asm!("/* {0} */", inout(reg_byte) value, options(pure, nomem, nostack, preserves_flags));
+        value
+    }
+}
+
+#[cfg(any(
+    target_arch = "arm",
+    target_arch = "aarch64",
+    target_arch = "riscv32",
+    target_arch = "riscv64"
+))]
+#[allow(asm_sub_register)]
+#[inline]
+fn optimizer_hide(mut value: u8) -> u8 {
+    // SAFETY: the input value is passed unchanged to the output, the inline assembly does nothing.
+    unsafe {
+        core::arch::asm!("/* {0} */", inout(reg) value, options(pure, nomem, nostack, preserves_flags));
+        value
+    }
+}
+
+#[cfg(not(any(
+    target_arch = "x86",
+    target_arch = "x86_64",
+    target_arch = "arm",
+    target_arch = "aarch64",
+    target_arch = "riscv32",
+    target_arch = "riscv64"
+)))]
+#[inline(never)] // This function is non-inline to prevent the optimizer from looking inside it.
+fn optimizer_hide(value: u8) -> u8 {
+    // SAFETY: the result of casting a reference to a pointer is valid; the type is Copy.
+    unsafe { core::ptr::read_volatile(&value) }
+}
+
+#[inline]
 fn constant_time_ne(a: &[u8], b: &[u8]) -> u8 {
     assert!(a.len() == b.len());
 
@@ -15,7 +54,9 @@ fn constant_time_ne(a: &[u8], b: &[u8]) -> u8 {
     for i in 0..len {
         tmp |= a[i] ^ b[i];
     }
-    tmp // The compare with 0 must happen outside this function.
+
+    // The compare with 0 must happen outside this function.
+    optimizer_hide(tmp)
 }
 
 /// Compares two equal-sized byte strings in constant time.
@@ -43,14 +84,15 @@ pub fn constant_time_eq(a: &[u8], b: &[u8]) -> bool {
 
 macro_rules! constant_time_ne_n {
     ($ne:ident, $n:expr) => {
-        // This function is non-inline to prevent the optimizer from looking inside it.
-        #[inline(never)]
+        #[inline]
         fn $ne(a: &[u8; $n], b: &[u8; $n]) -> u8 {
             let mut tmp = 0;
             for i in 0..$n {
                 tmp |= a[i] ^ b[i];
             }
-            tmp // The compare with 0 must happen outside this function.
+
+            // The compare with 0 must happen outside this function.
+            optimizer_hide(tmp)
         }
     };
 }
